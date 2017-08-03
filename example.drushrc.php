3d07810db61c1e50223da04f76c1f823df6ee0ff
Axel Rutz <axel.rutz@clever-systems.net>
2017-08-03 19:22:11 +0200

    Improved a lot

--- example.drushrc.php
+++ example.drushrc.php
@@ -3,9 +3,17 @@
  * @file example.drushrc.php
  */
 
+// Leave out unnecessary DB data.
 $options['skip-tables']['common'] = array('migration_*');
 $command_specific['sql-sync']['skip-tables-key'] = 'common';
 $options['structure-tables']['common'] = array('cache', 'cache_*', 'history', 'search_*', 'sessions', 'watchdog');
 $command_specific['sql-sync']['structure-tables-key'] = 'common';
 
+// Leave out unnecessary files.
 $command_specific['deploya-pull-data']['public-files-exclude-paths'] = 'advagg_css/:advagg_js/:css/:ctools/:feeds/:js/:languages/:styles/';
+
+// Always pull data after push, if not to live.
+# $command_specific['deploya-push-code']['pull-data'] = TRUE;
+
+// If we have stage file proxy, no need to pull public files.
+# $command_specific['deploya-pull-data']['public-files'] = FALSE;
