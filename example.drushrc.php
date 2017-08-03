32aee9a06d76585af8fb70c882df7ddb98da3d13
Axel Rutz <axel.rutz@clever-systems.net>
2017-08-03 20:19:41 +0200

    Improved drushrc

--- example.drushrc.php
+++ example.drushrc.php
@@ -3,11 +3,9 @@
  * @file example.drushrc.php
  */
 
-// Leave out unnecessary DB data.
-$options['skip-tables']['common'] = array('migration_*');
-$command_specific['sql-sync']['skip-tables-key'] = 'common';
-$options['structure-tables']['common'] = array('cache', 'cache_*', 'history', 'search_*', 'sessions', 'watchdog');
-$command_specific['sql-sync']['structure-tables-key'] = 'common';
+// Leave out unnecessary DB data in sql-sync and sql-dump.
+$options['skip-tables-list'] = array('migration_*');
+$options['structure-tables-list'] = array('cache', 'cache_*', 'history', 'search_*', 'sessions', 'watchdog');
 
 // Leave out unnecessary files.
 $command_specific['deploya-pull-data']['public-files-exclude-paths'] = 'advagg_css/:advagg_js/:css/:ctools/:feeds/:js/:languages/:styles/';
