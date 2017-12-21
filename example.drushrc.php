2e32c735c6a1eac651a9d07425defd8b4161522e
Axel Rutz <axel.rutz@clever-systems.net>
2017-12-21 15:27:11 +0100

    Fixed structure-tables

--- example.drushrc.php
+++ example.drushrc.php
@@ -4,8 +4,11 @@
  */
 
 // Leave out unnecessary DB data in sql-sync and sql-dump.
-$options['skip-tables-list'] = 'migration_*';
-$options['structure-tables-list'] = 'cache,cache_*,history,search_*,sessions,watchdog';
+$options['structure-tables']['common'] = array('cache', 'cache_*', 'cachetags', 'history', 'sessions', 'watchdog');
+$options['structure-tables-key'] = 'common';
+
+$options['skip-tables']['common'] = array('migrate_*');
+$options['skip-tables-key'] = 'common';
 
 // Leave out unnecessary files.
 $command_specific['deploya-pull-data']['public-files-exclude-paths'] = 'advagg_css/:advagg_js/:css/:ctools/:feeds/:js/:languages/:styles/';
